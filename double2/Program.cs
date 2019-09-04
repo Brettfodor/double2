970159bbf46ab0a351eba2c5e823fdc3bf139272
Brett Fodor <bmattmusic@gmail.com>
2019-09-04 19:45:06 -0400

    First commit

---
+++ double2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace double2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter a password.");
+
+            string password = Console.ReadLine();
+
+            Console.ReadLine();
+
+            Console.WriteLine("Please, re-enter your password");
+
+            string passwordTwo = Console.ReadLine();
+
+            Console.ReadLine();
+
+            if (password.Contains(passwordTwo))
+            {
+                Console.WriteLine("Congratulations, you successfully entered your password");
+            }
+        }
+    }
+}
